f6deef55102faf343c97baf2a5dadc05b5d03d73
Bhasker23 <shrimalibhaskar2398@gmail.com>
2022-09-08 16:14:12 +0530

    navbar assignment pushed

---
+++ script/index.js
@@ -0,0 +1,18 @@
+let small_screen_btn =document.getElementById("small-screen-btn");
+let cross=document.getElementById("cross");
+let list=document.getElementById("list");
+let temp=0;
+function dispaly_menu(){
+    if(temp%2==0){
+        
+        small_screen_btn.style.display="flex";
+        small_screen_btn.style.transition="5s";
+        cross.style.display="flex";
+        list.style.display="none";
+    }else{
+        small_screen_btn.style.display="none";
+        cross.style.display="none";
+        list.style.display="flex";
+    }
+    temp++;
+}
